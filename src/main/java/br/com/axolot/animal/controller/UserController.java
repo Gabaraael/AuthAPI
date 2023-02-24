b9e24e420af5ef3d2e83e7d487f77e3c7999675c
Gabaraael <Gabriel1998.gp45@gmail.com>
2023-02-24 20:26:04 -0300

    fix CORS

--- src/main/java/br/com/axolot/animal/controller/UserController.java
+++ src/main/java/br/com/axolot/animal/controller/UserController.java
@@ -5,6 +5,7 @@ import br.com.axolot.animal.dtos.UserLogin;
 import br.com.axolot.animal.dtos.UserPasswordChange;
 import br.com.axolot.animal.dtos.UserRegister;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/user")
+@CrossOrigin(origins = "*")
 public class UserController {
     private final UserService userService;
 
