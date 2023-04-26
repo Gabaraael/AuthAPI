c73cd8d9026101d647fd4464d124d9bace9d4825
gabaraael <gabriel1998.gp45@gmail.com>
2023-04-26 02:54:49 -0300

    disabling temporarily

--- src/main/java/br/com/axolot/animal/AuthApplication.java
+++ src/main/java/br/com/axolot/animal/AuthApplication.java
@@ -8,12 +8,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class AuthApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
-		
 	}
-	  @Override
-    protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
-        return builder.sources(AuthApplication.class);
-    }
+//	  @Override
+//    protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+//        return builder.sources(AuthApplication.class);
+//    }
 }
