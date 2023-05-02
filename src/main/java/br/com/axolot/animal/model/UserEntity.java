daa1d759993b331964cc1a13cacffd56f9baad4c
gabaraael <gabriel1998.gp45@gmail.com>
2023-05-02 02:57:26 -0300

    Implementando interface UserDetails para validar o usuário

--- src/main/java/br/com/axolot/animal/model/UserEntity.java
+++ src/main/java/br/com/axolot/animal/model/UserEntity.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,6 +16,8 @@ import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
@@ -20,7 +25,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "USER")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @Column(name = "USER_ID")
@@ -47,5 +52,29 @@ public class UserEntity {
     @PrePersist
     public void prePersist() {
         this.creationDate = LocalDateTime.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER_ROLE"));
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
